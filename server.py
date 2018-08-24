e8cf5cb53659cacd61260315628a05b75a7f8c21
Martin Häcker <mhaecker@mac.com>
2018-08-24 04:01:10 +0200

    Slightly changed default

--- server.py
+++ server.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 @app.route('/') # methods=['GET', 'POST']
 def index():
     tasks = Todo.from_lines(dedent("""
-        first task
-            second task with a longer description that would surely be shortened
-            third task status:unknown
-                with children
-            seventh task status:done
-            eight task status:doing
-        fourth task
-            fifth task
-                sixth task
-            ninth task status:doing
+        first task id:1
+            second task with a longer description that would surely be shortened id:2
+            third task id:3
+                with children id:4
+            seventh task status:done id:5
+            eight task status:doing id:6
+        fourth task id:7
+            fifth task id:8
+                sixth task id:9
+            ninth task status:doing id:10
     """))
     return render_template('index.html', tasks=tasks, tasks_json=list(map(lambda each: each.json, tasks)))
