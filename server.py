c218715672ad9d7a7f83d4426244f4f5d5a9a7f0
Martin Häcker <mhaecker@mac.com>
2018-08-24 04:36:34 +0200

    Show subtak contexts (owners)

--- server.py
+++ server.py
@@ -11,11 +11,11 @@ def index():
             second task with a longer description that would surely be shortened id:2
             third task id:3
                 with children id:4
-            seventh task status:done id:5
-            eight task status:doing id:6
+            seventh task status:done id:5 @mh
+            eight task status:doing id:6 @rb @juergen_klein
         fourth task id:7
             fifth task id:8
                 sixth task id:9
-            ninth task status:doing id:10
+            ninth task status:doing id:10 @fh
     """))
     return render_template('index.html', tasks=tasks, tasks_json=list(map(lambda each: each.json, tasks)))
