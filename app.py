59775b31a43d05c96c5504ac5ed87aa29f109734
Robert Buchholz <rbu@goodpoint.de>
2018-08-31 17:15:28 +0200

    Write json to file

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from todotxt import Todo
 from textwrap import dedent
 
 app = Flask(__name__)
 
-@app.route('/') # methods=['GET', 'POST']
+@app.route('/', methods=['GET'])
 def index():
     task = Todo.from_lines(open('todo.txt', encoding='utf8').read())
+    return render_template('index.html', task=task)
+
+@app.route('/', methods=['POST'])
+def update_todos():
+    task = Todo()
+    task.json = request.json
+    with open('todo.txt', encoding='utf8', mode='w') as f:
+        f.write(str(task))
     return render_template('index.html', task=task)
