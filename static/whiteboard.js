eca26acac0490f9d8f47f0a721350538cf8a35f8
Martin Häcker <mhaecker@mac.com>
2018-09-21 22:10:14 +0200

    Get whiteboard running again with improved dragdrop (now without sorting anymore)

--- static/whiteboard.js
+++ static/whiteboard.js
@@ -13,7 +13,20 @@ function generateClientUniqueID() {
 export default {
     template: `
     <div class=whiteboard v-bind:title="task.line">
+    
       <div class=header>
+        <nav aria-label=breadcrumb>
+          <ol class=breadcrumb>
+            <li class="breadcrumb-item"
+              v-for="crumb in breadcrumbs"
+              v-on:click.prevent.stop="browse(crumb)"
+              v-bind:key="crumb.id || generateClientUniqueID()"
+            >
+              <!-- FIXME href should point to actual ticket in tracker -->
+              <a href=#><span class=id v-if=crumb.id>#{{ crumb.id }}</span>{{ crumb.line || 'root' }}</a>
+            </li>
+          </ol>
+        </nav>
         <h1 v-if="(task.line || '').length"><a v-if="task.id" href="#" class="id" v-text="'#' + task.id"></a>{{ task.line }}</h1>
         <div class="column-headers">
           <h2 class="column header" 
@@ -24,82 +37,68 @@ export default {
             v-bind:key="columnName"
           >{{ columnName }} <span class="grandchild-count" >{{ countOfGrandChildrenInStatus(task, columnName) }}</span></h2>
         </div>
-
       </div>
-    </div>`,
-    template_old: `
-    <div class=whiteboard v-bind:title="task.line">
-    <div class=header>
-      <nav aria-label=breadcrumb>
-        <ol class=breadcrumb>
-          <li class="breadcrumb-item"
-            v-for="crumb in breadcrumbs"
-            v-on:click.prevent.stop="browse(crumb)"
-            v-bind:key="crumb.id || generateClientUniqueID()"
-          >
-            <!-- FIXME href should point to actual ticket in tracker -->
-            <a href=#><span class=id v-if=crumb.id>#{{ crumb.id }}</span>{{ crumb.line || 'root' }}</a>
-          </li>
-        </ol>
-      </nav>
-      // h1 and .column-headers
-    </div>
-    <div class="task"
-      v-for="child in task.children"
-      v-bind:title="child.line"
-      v-bind:class="{ is_done: child.is_done }"
-      v-bind:key="child.id || generateClientUniqueID()"
-    >
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col status" 
-            v-for="columnName in ['new', 'unknown', 'doing', 'done']"
-            v-if="columnName !== 'unknown' || countOfGrandChildrenInStatus(task, 'unknown') > 0" 
-            v-bind:class="[columnName]"
-            v-bind:key="columnName"
-          >
-            <h2 class=metadata 
-              v-if="columnName === 'new'"
-              v-on:click.prevent="toggleCollapsed($event, child)"
+      
+      <div class="task"
+        v-for="child in task.children"
+        v-bind:title="child.line"
+        v-bind:class="{ is_done: child.is_done }"
+        v-bind:key="child.id || generateClientUniqueID()"
+      >
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col status" 
+              v-for="columnName in ['new', 'unknown', 'doing', 'done']"
+              v-if="columnName !== 'unknown' || countOfGrandChildrenInStatus(task, 'unknown') > 0" 
+              v-bind:class="[columnName]"
+              v-bind:key="columnName"
             >
-              <!-- FIXME herf should point to bugtracker url -->
-              <a href="#" class="id" v-text="'#' + child.id"></a>
-              <span class="title" v-text="child.line"></span>
-              <a v-on:click.prevent.stop="browse(child)" href="#" class="browse button" title="Browse grandchildren Tasks">⏎</a>
-              <a v-on:click.prevent.stop="addChildTask(child)" href="#" class="add button" title="Add subtask">+</a>
-            </h2>
+              <h2 class=metadata 
+                v-if="columnName === 'new'"
+                v-on:click.prevent="toggleCollapsed($event, child)"
+              >
+                <!-- FIXME herf should point to bugtracker url -->
+                <a href="#" class="id" v-text="'#' + child.id"></a>
+                <span class="title" v-text="child.line"></span>
+                <a v-on:click.prevent.stop="browse(child)" href="#" class="browse button" title="Browse grandchildren Tasks">⏎</a>
+                <a v-on:click.prevent.stop="addChildTask(child)" href="#" class="add button" title="Add subtask">+</a>
+              </h2>
 
-            <h2 class=metadata  v-if="columnName === 'done'">
-              <span class=flexspace></span>
-              <span class="stats" v-text="child.children.done.length + '/' + subtaskCount(child)"></span>
-            </h2>
-            <draggable class="drag-container"
-              v-model="childrenInStatus(child, columnName)"
-              v-bind:options="{group:String(cuid(child))}"
-            >
-              <!-- REFACT use v-key to allow animations between orderings and column change (not sure this is possible) -->
-              <div class="col subtask" 
-                v-for="grandChild in childrenInStatus(child, columnName)" 
-                v-bind:title="grandChild.line"
-                v-bind:key="grandChild.id || generateClientUniqueID()"
-            >
-                <h3>
-                  <span class="metadata">
-                      <a href="#" class="edit button" title="Edit this task">✎</a>
-                      <span class="child-indicator" title="Has child tasks" v-if="grandChild.children.length > 0"></span>
-                      <a href="#" class="id" v-text="'#' + grandChild.id" title="External Link to task"></a>
-                  </span>
-                  <span class="title" v-text="grandChild.line"></span>
-                  <span class="contexts" v-text="grandChild.contexts.join(', ')"></span>
-                </h3>
-              </div>
-            </draggable>
+              <h2 class=metadata  v-if="columnName === 'done'">
+                <span class=flexspace></span>
+                <span class="stats" v-text="childrenInStatus(child, 'done').length + '/' + child.children.length"></span>
+              </h2>
+              
+              <draggable class="drag-container"
+                v-model="childrenInStatus(child, columnName)"
+                v-bind:options="{ group: String(cuid(child)), sort: false }"
+                v-on:end="onDragEnd"
+                v-bind:data-status="columnName"
+              >
+                <!-- REFACT use v-key to allow animations between orderings and column change (not sure this is possible) -->
+                <div class="col subtask" 
+                  v-for="grandChild in childrenInStatus(child, columnName)" 
+                  v-bind:title="grandChild.line"
+                  v-bind:key="grandChild.id || generateClientUniqueID()"
+              >
+                  <h3>
+                    <span class="metadata">
+                        <a href="#" class="edit button" title="Edit this task">✎</a>
+                        <span class="child-indicator" title="Has child tasks" v-if="grandChild.children.length > 0"></span>
+                        <a href="#" class="id" v-text="'#' + grandChild.id" title="External Link to task"></a>
+                    </span>
+                    <span class="title" v-text="grandChild.line"></span>
+                    <span class="contexts" v-text="grandChild.contexts.join(', ')"></span>
+                  </h3>
+                </div>
+              </draggable>
+              
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-    `,
+      
+    </div>`,
   props: {
     rootTask: {
       type: Object,
@@ -160,6 +159,7 @@ export default {
       task.children.new.push({
         line: "",
         id: "",
+        status: 'new',
         is_done: false,
         projects: [],
         contexts: [],
@@ -172,7 +172,13 @@ export default {
       var $task = $($event.currentTarget).closest(".task");
       $task.toggleClass("toggle-collapsed");
     },
-
+    
+    onDragEnd: function(event) {
+      // whats the right way to get at the vue element? __vue__ seems fishy
+      const draggedTask = event.from.__vue__.context.element
+      draggedTask.status = event.to.dataset.status
+    },
+    
     browse: function(task) {
       const index = this.breadcrumbs.indexOf(task);
       if (-1 === index) {
@@ -183,7 +189,9 @@ export default {
       }
       this.task = task;
     },
-
+    
+    // REFACT this should be a server side given ID in almost all cases
+    // i.e. I need a concept to get rid of these asap, even if they are needed as stopgaps
     cuid: function(task) {
       if (undefined === task.cuid) {
         task.cuid = generateClientUniqueID();
