2d83395892efc99a5491e50f8be1fdb8172e662f
Dan Levin <dan@badpacket.in>
2019-01-16 11:34:48 +0100

    Include more guidance for docker-prioritized restructuring the dev environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,13 @@
 
 
 ####
+# There's an argument to be made that virtualenv isn't necessary and shouldn't
+# be used in docker
+pyvenv . 
+source bin/activate
 # These should actually be done at container-build time, but are difficult to
 # encapsulate (while also supporing volumes) given the current project
 # structure layout.
-pyvenv .
-source bin/activate
 pip install -r requirements.txt
 poetry install
 yarn install
