f2e9a1e80cf5e0d7238f0498177666b30eb06cd0
Martin Häcker <mhaecker@mac.com>
2020-12-21 20:30:42 +0100

    Extract compiler into it's own function to enable additional compilers.
    Goal would be to have a single file that contains vue model code, templates and scss

--- static/utils.js
+++ static/utils.js
@@ -1,26 +1,33 @@
-export default function importSCSS(scssFileName) {
+export function importSCSS(scssFileName) {
     importCSS(scssFileName.replace(/\.scss$/, '.css')).addEventListener('error', event => {
-        if (!importSCSS.compiler) {
-            // REFACT is there a way to express that this depends on 
-            // import sassModule from '/static/vendor/sass.js/sass.js'
-            // can't use straight import, as sass.js bombs when it has no global object
-            if (typeof Sass === 'function') {
-                importSCSS.compiler = new Sass()
-            }
-            else {
-                return // just skip sass compilation in the karma runner for now
-            }
-        }
         fetch(scssFileName)
             .then(response => response.text())
-            .then(function (scssString) {
-                importSCSS.compiler.compile(scssString, function (css) {
-                    let style = document.createElement('style')
-                    style.appendChild(document.createTextNode(css.text))
-                    document.head.appendChild(style)
-                })
+            .then(compileAndActivate)
+    })
+}
+
+// use this as a template tag scss`some multiline scss string`
+export function scss(scssText) {
+	compileAndActivate(String.raw(scssText))
+}
+
+function compileAndActivate(scssString) {
+    if (!compileAndActivate.compiler) {
+        // REFACT is there a way to express that this depends on 
+        // import sassModule from '/static/vendor/sass.js/sass.js'
+        // can't use straight import, as sass.js bombs when it has no global object
+        if (typeof Sass === 'function') {
+            compileAndActivate.compiler = new Sass()
+        }
+        else {
+            return // just skip sass compilation in the karma runner for now
+        }
+    }
     
-            })
+    compileAndActivate.compiler.compile(scssString, function (css) {
+        let style = document.createElement('style')
+        style.appendChild(document.createTextNode(css.text))
+        document.head.appendChild(style)
     })
 }
 
@@ -37,4 +44,8 @@ export function importCSS(cssFileName) {
     //         style.appendChild(document.createTextNode(cssString))
     //         document.head.appendChild(style)
     //     })
+}
+
+export function vueTemplate(templateString) {
+    return String.raw(templateString)
 }
